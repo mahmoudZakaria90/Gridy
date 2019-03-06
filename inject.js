bab99501529505fec40ac0b12811c13c49daac3a
Mahmoud Zakria <me@zakcoding.com>
2019-03-06 04:55:35 +0100

    last update, no conflict with css

--- inject.js
+++ inject.js
@@ -9,13 +9,13 @@ var getFromClientCB = ( { width, rebuild, state, opacity, color } ) => {
     base = 12,
     wrap = document.createElement('div');
 
-    wrap.id = "grid-wrap";
-    wrap.className = width ? "grid-wrap is-container" : "grid-wrap";
+    wrap.id = "gridy-wrap";
+    wrap.className = width ? "gridy-wrap is-container" : "gridy-wrap";
     wrap.style.width =  wrapWidth + 'px';
     wrap.style.opacity = opacity ? opacity : 1;
     for (var i = 0; i < base; i++) {
         const col = document.createElement('div');
-        col.className = `cols cols-${(i + 1)}`;
+        col.className = `gridy-cols gridy-cols-${(i + 1)}`;
         col.style.backgroundColor = color;
         wrap.appendChild(col);
     }

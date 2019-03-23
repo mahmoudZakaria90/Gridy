8bc5c4372ac44d9ee69d8ce775c49f83fbd5e586
Mahmoud Zakria <me@zakcoding.com>
2019-03-23 12:35:05 +0100

    latest update, trying to implement the state manager

--- inject.js
+++ inject.js
@@ -1,37 +1,41 @@
-var getFromClient = () =>
-    new Promise( resolve =>
-        chrome.storage.sync.get( ['width', 'rebuild', 'state', 'opacity', 'color'], data => resolve( data ) ) );
-
-var getFromClientCB = ( { width, rebuild, state, opacity, color } ) => {
-    const minWidth = 320;
-    const maxWidth = window.innerWidth;
-    const wrapWidth = ( width < minWidth || width > maxWidth  ) ? maxWidth : width,
+chrome.runtime.onMessage.addListener(function({
+  width,
+  rebuild,
+  state,
+  opacity,
+  color
+}) {
+  const minWidth = 320;
+  const maxWidth = window.innerWidth;
+  const wrapWidth = width < minWidth || width > maxWidth ? maxWidth : width,
     base = 12,
-    wrap = document.createElement('div');
+    wrap = document.createElement("div");
 
-    wrap.id = "gridy-wrap";
-    wrap.className = width ? "gridy-wrap is-container" : "gridy-wrap";
-    wrap.style.width =  wrapWidth + 'px';
-    wrap.style.opacity = opacity ? opacity : 1;
-    for (var i = 0; i < base; i++) {
-        const col = document.createElement('div');
-        col.className = `gridy-cols gridy-cols-${(i + 1)}`;
-        col.style.backgroundColor = color;
-        wrap.appendChild(col);
+  wrap.id = "gridy-wrap";
+  wrap.className = width ? "gridy-wrap is-container" : "gridy-wrap";
+  wrap.style.width = wrapWidth + "px";
+  wrap.style.opacity = opacity ? opacity : 1;
+  for (var i = 0; i < base; i++) {
+    const col = document.createElement("div");
+    col.className = `gridy-cols gridy-cols-${i + 1}`;
+    col.style.backgroundColor = color;
+    wrap.appendChild(col);
+  }
+  const wrapAlready = document.getElementById(wrap.id);
+
+  if (rebuild) {
+    if (wrapAlready) document.body.removeChild(wrapAlready);
+
+    document.body.appendChild(wrap);
+  } else if (!state) {
+    if (wrapAlready) {
+      document.body.removeChild(wrapAlready);
     }
-    const wrapAlready = document.getElementById(wrap.id);
-
-    if( rebuild )
-    {
-        if( wrapAlready ) document.body.removeChild(wrapAlready);
-
-        document.body.appendChild(wrap);
-
-    } else if( !state ) {
-        if( wrapAlready ) {
-            document.body.removeChild(wrapAlready);
-        }
-    }
-}
-
-getFromClient().then( getFromClientCB )
+  }
+  // chrome.runtime.sendMessage({
+  //   width,
+  //   state,
+  //   opacity,
+  //   color
+  // });
+});
--- popup.js
+++ popup.js
@@ -1,56 +1,89 @@
-(function(){
-    let gridToggleBtn = document.querySelector('#grid-toggle-btn');
-    let widthInput = document.querySelector('#widthInput');
-    let opacityRange = document.querySelector('#opacityRange');
-    let colorPicker = document.querySelector('#colorPicker');
-    let state = false;
+(function() {
+  let gridToggleBtn = document.querySelector("#grid-toggle-btn");
+  let widthInput = document.querySelector("#widthInput");
+  let opacityRange = document.querySelector("#opacityRange");
+  let colorPicker = document.querySelector("#colorPicker");
+  let state = false;
 
-    let chromeInject = function( key, file ) {
-        chrome.tabs[key]({
-            file
-        });
+  let chromeInject = function(key, file) {
+    chrome.tabs[key]({
+      file
+    });
+  };
+
+  function sendToActiveTab(target) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, target);
+    });
+  }
+  function setButtonState(target, state) {
+    if (state) {
+      target.className += " is-on";
+    } else {
+      target.className = "grid-toggle-btn";
     }
+  }
 
-    let chromeSync = function(key, value, rebuild) {
-        chrome.storage.sync.set({
-            [key]: value ? value : null,
-            rebuild: state
-        });
-    }
+  let chromeSync = function(key, value, rebuild) {
+    const values = {
+      width: widthInput.value,
+      opacity: opacityRange.value,
+      color: colorPicker.value
+    };
+    sendToActiveTab({
+      ...values,
+      [key]: value ? value : null,
+      rebuild: state
+    });
+  };
 
-    let chromeSyncCB = function(key) {
-        return function() {
-            chromeSync(key, this.value, true);
-            chromeInject('executeScript', 'inject.js');
-        }
-    }
+  let chromeSyncCB = function(key) {
+    return function() {
+      chromeSync(key, this.value, true);
+      chromeInject("executeScript", "inject.js");
+    };
+  };
 
-    let toggleCB = function(e){
-        state = !state;
-        if( state ) {
-            e.currentTarget.className += ' is-on';
-        } else {
-            e.currentTarget.className = 'grid-toggle-btn';
-        }
-        chrome.storage.sync.set( {
-            width: widthInput.value ? Number(widthInput.value) : null,
-            opacity: opacityRange.value ? opacityRange.value : null,
-            color: colorPicker.value ? colorPicker.value : null,
-            rebuild: state,
-            state
-        } );
-        chromeInject('executeScript', 'inject.js');
-    }
+  let toggleCB = function(e) {
+    state = !state;
+    setButtonState(e.currentTarget, state);
+    sendToActiveTab({
+      width: widthInput.value ? Number(widthInput.value) : null,
+      opacity: opacityRange.value ? opacityRange.value : null,
+      color: colorPicker.value ? colorPicker.value : null,
+      rebuild: state,
+      state
+    });
+    chromeInject("executeScript", "inject.js");
+  };
 
-    gridToggleBtn.onclick = toggleCB;
+  gridToggleBtn.onclick = toggleCB;
 
-    gridToggleBtn.addEventListener('mousedown', chromeInject.bind(null, 'insertCSS', 'inject.css'), { once : true });
+  gridToggleBtn.addEventListener(
+    "mousedown",
+    chromeInject.bind(null, "insertCSS", "inject.css"),
+    { once: true }
+  );
 
+  widthInput.oninput = chromeSyncCB("width");
 
-    widthInput.oninput = chromeSyncCB('width');
+  opacityRange.oninput = chromeSyncCB("opacity");
 
-    opacityRange.oninput = chromeSyncCB('opacity');
-
-    colorPicker.oninput = chromeSyncCB('color');
-
+  colorPicker.oninput = chromeSyncCB("color");
+  //   chrome.runtime.onMessage.addListener(function({
+  //     width,
+  //     state,
+  //     opacity,
+  //     color
+  //   }) {
+  //     widthInput.value = width;
+  //     state = state;
+  //     opacityRange.value = opacity;
+  //     colorPicker.value = color;
+  //     if (state) {
+  //       gridToggleBtn.className += " is-on";
+  //     } else {
+  //       gridToggleBtn.className = "grid-toggle-btn";
+  //     }
+  //   });
 })();
